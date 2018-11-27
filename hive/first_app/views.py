669e85bf01fb887d4ea687724eba4bf59abb9470
Nathan Adda <nathanadda@MacBook-Pro-de-Nathan.local>
2018-11-27 10:45:24 +0200

    Navbar creation

--- hive/first_app/views.py
+++ hive/first_app/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse('Hello Project Hive\n Pour vous dire raph est une pute')
+	return render(request, 'index.html')
