5e001c17b4a7b2a981d69454bdb6b6abfd571de8
Nathan Adda <nathanadda@me.com>
2018-11-29 00:09:16 +0200

    Edit page done.

--- hive/first_app/views.py
+++ hive/first_app/views.py
@@ -1,12 +1,13 @@
 
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound
-from first_app.forms import NewTweetForm, ProfileEditForm
+from first_app.forms import NewTweetForm, ProfileEditForm, PasswordEditForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from first_app.models import UserProfileInfo, Tweet
 from django.contrib.auth.models import User
 import datetime
+from django.contrib.auth.hashers import check_password, make_password
 
 # Create your views here.
 	
@@ -52,22 +53,63 @@ def publish_a_tweet(request, user_id):
 
 @login_required
 def edit_page(request):
-	if request.method == 'POST':
-		edit_form = ProfileEditForm(request.POST)
-		if edit_form.is_valid():
-			clean_data = edit_form.clean()
-			pass
-			# UserProfileInfo.objects.
+	user = request.user
+	form_error = False
+	password_error = False
+	userprofile = UserProfileInfo.objects.get(user=user)
+	default_data = {'email': user.email, 'username': user.username, 'bio': userprofile.bio}
 
+	if request.method == 'POST':
+		if 'edit_profile' in request.POST:
+			edit_form = ProfileEditForm(request.POST)
+			if edit_form.is_valid():
+				clean_data = edit_form.clean()
+				if user.username == clean_data['username'] or (user.username != clean_data['username'] and len(User.objects.filter(username=clean_data['username'])) == 0 ):
+					user.username = clean_data['username']
+					user.email = clean_data['email']
+					userprofile.bio = clean_data['bio']
+
+					userprofile.save()
+					user.save()
+					return home(request)
+				else:
+					password_form = PasswordEditForm(label_suffix=' : ')
+					edit_form =  ProfileEditForm(default_data, label_suffix=' : ')
+					form_error = True
+			else:
+				print(edit_form.errors)
+
+		elif 'change_pass' in request.POST:
+			password_form = PasswordEditForm(request.POST)
+			if password_form.is_valid():
+				clean_data = password_form.clean()
+				if check_password(clean_data['old_password'], user.password):
+					# print(clean_data['new_password'])
+					new_pass = make_password(clean_data['new_password'])
+					# print(new_pass)
+					user.password = new_pass
+					# print(user.password)
+					user.save()
+					return home(request)
+				else:
+					password_error = True
+					password_form = PasswordEditForm(label_suffix=' : ')
+					edit_form =  ProfileEditForm(default_data, label_suffix=' : ')
+
+			else:
+				print(password_form.errors)
+				password_form = PasswordEditForm(label_suffix=' : ')
+				edit_form = ProfileEditForm(default_data, label_suffix=' : ')
 
 	else:
-		user = request.user
-		userprofile = UserProfileInfo.objects.get(user=user)
-		default_data = {'email': user.email, 'username': user.username, 'bio': userprofile.bio}
-		edit_form = ProfileEditForm(default_data, label_suffix=': ')
+		edit_form = ProfileEditForm(default_data, label_suffix=' : ')
+		password_form = PasswordEditForm(label_suffix=' : ')
 
 	return render(request, 'profile_edit.html', {
 			'edit_form': edit_form,
+			'form_error': form_error,
+			'password_form': password_form,
+			'password_error': password_error
 		})
 
 
